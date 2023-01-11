1edd65c9d8ef30d32ef1a70b918f72ed227ac0ef
Fernando-A-Rocha <frocha2001@gmail.com>
2023-01-11 12:10:38 +0000

    update 2.0

---
+++ [dependencies]/newmodels/_config.lua
@@ -0,0 +1,37 @@
+--[[
+	Author: Fernando
+
+	_config.lua
+
+	All global config variables are in this file:
+]]
+
+----------- GENERAL SCRIPT CONFIGURATION -----------
+
+-- saves custom element model ID in its element data under the following names
+dataNames = {
+	ped = "skinID", player = "skinID", -- must be the same
+	vehicle = "vehicleID",
+	object = "objectID",
+}
+
+-- saves the element's custom model's base model ID
+-- useful for getting a vehicle's base model to fetch its original handling, etc
+baseDataName = "baseID"
+
+
+----------- DEBUGGING CONFIGURATION -----------
+
+START_STOP_MESSAGES = false -- enable resouce start/stop automatic chat messages
+SEE_ALLOCATED_TABLE = false -- automatically executes /allocatedids on startup
+ENABLE_DEBUG_MESSAGES = false -- toggle all debug console messages
+CHAT_DEBUG_MESSAGES = false -- make debug console messages to go chatbox (better readability imo)
+
+
+----------- OPTIONAL CONFIGURATION -----------
+
+-- NandoCrypt | https://github.com/Fernando-A-Rocha/mta-nandocrypt
+-- The decrypt function needs to be named ncDecrypt inside a decrypter script (named nando_decrypter by default)
+
+ENABLE_NANDOCRYPT = false
+NANDOCRYPT_EXT = ".nandocrypt"
